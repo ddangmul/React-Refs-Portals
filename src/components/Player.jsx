18586be42463d07a9f4c2d6471b30c07365a334d
ddangmul <j.ddangmul@gmail.com>
2024-11-11 18:25:07 +0900

    참조(Refs)로 상태값 playerName 관리

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,19 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
-  const [playerName, setPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  const inputedPlayerName = useRef();
 
-  function handleChange(event) {
-    setPlayerName(event.target.value);
-  }
+  const [playerName, setPlayerName] = useState("");
 
   function handleClick() {
-    setSubmitted(true);
+    setPlayerName(inputedPlayerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? playerName : "unknown entity"}</h2>
+      <h2>Welcome {playerName ? playerName : "unknown enntity"} </h2>
       <p>
-        <input type="text" onChange={handleChange} value={playerName} />
+        <input ref={inputedPlayerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
