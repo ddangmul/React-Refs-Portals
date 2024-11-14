d45864c80f4a76a55b1deb44ca4f839c68da30c9
ddangmul <j.ddangmul@gmail.com>
2024-11-14 18:34:50 +0900

    참조로 타이머 멈추기 & forwardRef로 모달창 추가

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,39 +1,45 @@
 import { useState, useRef } from "react";
-
-// let timer;
+import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+  const dialog = useRef();
+
   const [timerExpired, setTimerExpired] = useState(false);
   const [timerStarted, setTimerStarted] = useState(false);
-  const timer = useRef();
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
-    }, targetTime * 1000); // 밀리초로 반환하기 위해 * 1000
+      dialog.current.showModal();
+    }, targetTime * 1000);
+
     setTimerStarted(true);
   }
 
   function handleStop() {
-    setTimerStarted(false);
+    // setTimerStarted(false);
     clearTimeout(timer.current);
   }
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired && <p>You lost!</p>}
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? "s" : ""}
-      </p>
-      <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>
-          {timerStarted ? "Stop" : "Start"} Challenge
-        </button>
-      </p>
-      <p className={timerStarted ? "active" : "undefined"}>
-        {timerStarted ? "Time is running..." : "Timer inactive"}
-      </p>
-    </section>
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <section className="challenge">
+        <h2>{title}</h2>
+        {timerExpired && <p>You lost!</p>}
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? "active" : "undefined"}>
+          {timerStarted ? "Time is running..." : "Timer inactive"}
+        </p>
+      </section>
+    </>
   );
 }
