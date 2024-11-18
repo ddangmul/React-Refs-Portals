4f701cbf762835fedb9f1e8fb1952ed70cd10723
ddangmul <j.ddangmul@gmail.com>
2024-11-18 17:52:26 +0900

    setTimeout을 setInterval로 변경/잔여시간을 상태관리 - win, lost 모두 모달창 활성화

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -5,21 +5,37 @@ export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
 
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
+  // const [timerStarted, setTimerStarted] = useState(false);
+  // const [timerExpired, setTimerExpired] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    // 졌을 경우
+    clearInterval(timer.current);
+    setTimeRemaining(targetTime * 1000);
+    dialog.current.open();
+  }
 
   function handleStart() {
-    setTimerStarted(true);
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      dialog.current.open();
-      setTimerStarted(false);
-    }, targetTime * 1000);
+    // setTimerStarted(true);
+    // timer.current = setTimeout(() => {
+    //   setTimerExpired(true);
+    //   dialog.current.open();
+    //   setTimerStarted(false);
+    // }, targetTime * 1000);
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
-    setTimerStarted(false);
+    // 이겼을 경우
+    // clearTimeout(timer.current);
+    clearInterval(timer.current);
+    // setTimerStarted(false);
+    dialog.current.open();
   }
 
   return (
@@ -27,18 +43,26 @@ export default function TimerChallenge({ title, targetTime }) {
       <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
       <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>You lost!</p>}
+        {timerIsActive > 0 && <p>You lost!</p>}
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timerIsActive ? "active" : "undefined"}>
+          {timerIsActive ? "Time is running..." : "Timer inactive"}
+        </p>
+        {/* <p>
           <button onClick={timerStarted ? handleStop : handleStart}>
             {timerStarted ? "Stop" : "Start"} Challenge
           </button>
         </p>
         <p className={timerStarted ? "active" : "undefined"}>
           {timerStarted ? "Time is running..." : "Timer inactive"}
-        </p>
+        </p> */}
       </section>
     </>
   );
